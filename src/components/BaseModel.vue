87ad6190409070eb339ec94aea3bcdabc9b7985b
Jared <shixuran@qq.com>
2022-09-01 19:04:04 +0800

    create a popup component  of information

---
+++ src/components/BaseModel.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-show="modelActive"
+        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
+        <div v-if="modelActive" class="bg-white max-w-screen-md self-start px-7 py-5 mt-32">
+            <slot></slot>
+            <button @click="$emit('close-model')" class="text-white mt-8 bg-weather-primary py-2 px-6">
+                关闭
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+// 关闭按钮
+defineEmits(["close-model"])
+// 控制组件显示
+defineProps({
+    modelActive: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,6 +1,7 @@
 <template>
     <header class="sticky top-0 bg-weather-primary shadow-lg">
         <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
+            <!-- 太阳图标/标题 -->
             <RouterLink :to="{ name: 'home' }">
                 <div class="flex items-center gap-3">
                     <i class="fa-solid fa-sun text-2xl"></i>
@@ -8,14 +9,53 @@
                 </div>
             </RouterLink>
 
+            <!-- 信息/添加城市 -->
             <div class="flex gap-3 flex-1 justify-end">
-                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+                    @click="toggleModel"></i>
                 <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
             </div>
+
+            <BaseModel :model-active="modelActive" @close-model="toggleModel">
+                <div class="text-black">
+                    <h1 class="text-2xl mb-1">关于:</h1>
+                    <p class="mb-4">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+
+                    <h2 class="text-2xl">如何使用:</h2>
+                    <ol class="list-decimal list-inside mb-4">
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, asperiores quaerat.
+                        </li>
+                        <li>
+                            Lorem ipsum dolor. Voluptatibus sit amet consectetur adipisicing elit, asperiores quaerat.
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, asperiores.
+                            Voluptatibus quaerat.
+                        </li>
+                    </ol>
+
+                    <h2 class="text-2xl">删除城市:</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati maiores, quo doloremque
+                        minus quisquam odio, minima deleniti, exercitationem tenetur sequi voluptates modi ipsa eveniet
+                        iure.
+                    </p>
+                </div>
+            </BaseModel>
         </nav>
     </header>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { RouterLink } from 'vue-router';
+import BaseModel from './BaseModel.vue';
+
+const modelActive = ref(null);
+const toggleModel = () => {
+    modelActive.value = !modelActive.value;
+}
 </script>
