0045c8c9326425c4e3468c670b714500aaf4544c
Jared <shixuran@qq.com>
2022-09-01 19:42:17 +0800

    add animation to the popup  and teleport it out

--- src/components/BaseModel.vue
+++ src/components/BaseModel.vue
@@ -1,13 +1,19 @@
 <template>
-    <div v-show="modelActive"
-        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
-        <div v-if="modelActive" class="bg-white max-w-screen-md self-start px-7 py-5 mt-32">
-            <slot></slot>
-            <button @click="$emit('close-model')" class="text-white mt-8 bg-weather-primary py-2 px-6">
-                关闭
-            </button>
-        </div>
-    </div>
+    <Teleport to="body">
+        <Transition name="model-outer">
+            <div v-show="modelActive"
+                class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
+                <Transition name="model-inner">
+                    <div v-if="modelActive" class="bg-white max-w-screen-md self-start px-7 py-5 mt-32">
+                        <slot></slot>
+                        <button @click="$emit('close-model')" class="text-white mt-8 bg-weather-primary py-2 px-6">
+                            关闭
+                        </button>
+                    </div>
+                </Transition>
+            </div>
+        </Transition>
+    </Teleport>
 </template>
 
 <script setup>
@@ -22,5 +28,34 @@ defineProps({
 })
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+/* 打开关闭过程中 */
+.model-outer-enter-active,
+.model-outer-leave-active {
+    transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+/* 淡入淡出 */
+.model-outer-enter-from,
+.model-outer-leave-to {
+    opacity: 0;
+}
+
+.model-inner-enter-active {
+    transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+
+.model-inner-leave-active {
+    transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+/* 缩放 */
+.model-inner-enter-from {
+    opacity: 0;
+    transform: scale(0.8);
+}
+
+.model-inner-leave-to {
+    transform: scale(0.8);
+}
 </style>
