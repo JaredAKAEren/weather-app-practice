54b9db14b7dbfe03ef415babbbd8696402688cd3
Jared <shixuran@qq.com>
2022-09-05 00:35:09 +0800

    show search results with API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,87 @@
 <template>
   <main class="container text-white">
     <div class="pt-4 mb-8 relative">
-      <input type="text" v-model="searchQuery" placeholder="输入需要添加的城市"
-        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E70]" />
+      <input
+        type="text"
+        v-model="searchQuery"
+        @input="getSearchResults"
+        placeholder="输入需要添加的城市"
+        class="
+          py-2
+          px-1
+          w-full
+          bg-transparent
+          border-b
+          focus:border-weather-secondary
+          focus:outline-none
+          focus:shadow-[0px_1px_0_0_#004E70]
+        "
+      />
+      <ul
+        v-if="qweatherSearchResults != null"
+        class="
+          absolute
+          bg-weather-secondary
+          w-full
+          shadow-md
+          py-2
+          px-2
+          top-[66px]
+        "
+      >
+        <p v-if="searchError" class="py-2">抱歉，出现了一些错误，请重试。</p>
+        <p
+          v-if="!searchError && qweatherSearchResults.length === 0"
+          class="py-2"
+        >
+          查询的地区不存在，请重试。
+        </p>
+        <template v-else>
+          <li
+            v-for="searchResult in qweatherSearchResults"
+            :key="searchResult.id"
+            class="py-2 cursor-pointer"
+          >
+            <!-- 名称 - 上级行政区(), 一级行政区域, 国家 -->
+            {{
+              `${searchResult.name} - ${searchResult.adm2}, ${searchResult.adm1}, ${searchResult.country}`
+            }}
+          </li>
+        </template>
+      </ul>
     </div>
   </main>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
+import axios from "axios";
 
-const searchQuery=ref("");
+const qweatherAPIKey = "5a7251cd3b2e4396b101716cf2a9a74b";
+const searchQuery = ref("");
+const queryTimeout = ref(null);
+const qweatherSearchResults = ref(null);
+const searchError = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      try {
+        const result = await axios.get(
+          `https://geoapi.qweather.com/v2/city/lookup?location=${searchQuery.value}&key=${qweatherAPIKey}`
+        );
+        if (result.data.code == 200) {
+          qweatherSearchResults.value = result.data.location;
+        } else {
+          qweatherSearchResults.value = "";
+        }
+      } catch {
+        searchError.value = true;
+      }
+      return;
+    }
+    qweatherSearchResults.value = null;
+  }, 300);
+};
 </script>
