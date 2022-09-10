8755471a65a7ef8950246a614402694d5c8396fd
Jared <shixuran@qq.com>
2022-09-10 20:17:08 +0800

    添加了一些注释

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -10,6 +10,8 @@ const route = useRoute();
 // 城市信息查询API_Key
 const qweatherAPIKey = "5a7251cd3b2e4396b101716cf2a9a74b";
 
+
+// 获取当前城市的实时天气信息
 const getWeatherData = async () => {
   await axios
     .get(
@@ -25,5 +27,6 @@ const getWeatherData = async () => {
     });
 };
 
+// 调用getWeatherData方法获取并保存实时天气信息
 const weatherData = await getWeatherData();
 </script>
