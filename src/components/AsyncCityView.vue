8f57ffe8293cf265be4098316e42a9f5761dc258
Jared <shixuran@qq.com>
2022-09-12 18:15:11 +0800

    实现了查看未来24小时天气预报的功能

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -5,19 +5,54 @@
       <p>点击 “+” 添加当前城市到首页。</p>
     </div>
 
-    <!-- 天气信息 -->
+    <!-- 实时天气 -->
     <div class="flex flex-col items-center py-10 text-white">
       <!-- 城市名称 -->
       <h1 class="text-4xl mb-5">{{ route.params.city }}</h1>
       <!-- 时间 -->
-      <p class="mb-10">{{ weatherData.obsTime }}</p>
+      <p class="mb-10">{{ weatherDataNow.obsTime }}</p>
       <!-- 温度 -->
-      <p class="text-8xl mb-8">{{ weatherData.temp }}&deg;</p>
+      <p class="text-8xl mb-8">{{ weatherDataNow.temp }}&deg;</p>
+      <!-- 体感温度 -->
+      <p>体感温度{{ weatherDataNow.feelsLike }}&deg;</p>
+      <!-- 天气状况 -->
+      <p>{{ weatherDataNow.text }}</p>
       <!-- 天气图标 -->
-      <div class="text-5xl">
-        <i :class="`qi-${weatherData.icon}-fill`"></i>
+      <div class="text-5xl mt-4">
+        <i :class="`qi-${weatherDataNow.icon}-fill`"></i>
       </div>
     </div>
+
+    <!-- 分割线 -->
+    <hr class="border-white w-full border-opacity-10 border" />
+
+    <!-- 逐小时天气 -->
+    <div class="max-w-screen-md w-full py-12">
+      <div class="text-white mx-8">
+        <h2 class="mb-4">未来24小时天气</h2>
+        <div class="flex gap-10 overflow-x-scroll">
+          <div
+            v-for="(hourData, index) in weatherDataHourly"
+            :key="index"
+            class="flex flex-col items-center gap-3"
+          >
+            <!-- 时间 -->
+            <p class="text-sm">
+              {{ hourData.fxTime.substr(11, 5) }}
+            </p>
+            <!-- 图标 -->
+            <div class="text-2xl">
+              <i :class="`qi-${weatherDataNow.icon}-fill`"></i>
+            </div>
+            <!-- 温度 -->
+            <p>{{ hourData.temp }}&deg;</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- 逐天天气 -->
+    <div></div>
   </div>
 </template>
 
@@ -35,8 +70,8 @@ const obsTimeFormat = inject("obsTimeFormat");
 const qweatherAPIKey = "5a7251cd3b2e4396b101716cf2a9a74b";
 
 // 获取当前城市的实时天气信息
-const getWeatherData = async () => {
-  const weatherData = ref(null);
+const getWeatherDataNow = async () => {
+  const weatherDataNow = ref(null);
 
   await axios
     .get(
@@ -44,16 +79,41 @@ const getWeatherData = async () => {
     )
     .then((response) => {
       if (response.data.code == 200) {
-        weatherData.value = response.data.now;
-        weatherData.value.obsTime = obsTimeFormat(weatherData.value.obsTime);
+        weatherDataNow.value = response.data.now;
+        weatherDataNow.value.obsTime = obsTimeFormat(
+          weatherDataNow.value.obsTime
+        );
       }
     })
     .catch((error) => {
       console.log(error);
     });
-  return weatherData.value;
+  return weatherDataNow.value;
 };
+// 调用getWeatherDataNow方法获取并保存实时天气信息
+const weatherDataNow = await getWeatherDataNow();
 
-// 调用getWeatherData方法获取并保存实时天气信息
-const weatherData = await getWeatherData();
+// 获取当前城市的每小时天气信息
+const getWeatherDataHourly = async () => {
+  const weatherDataHourly = ref(null);
+
+  await axios
+    .get(
+      `https://devapi.qweather.com/v7/weather/24h?location=${route.query.id}&key=${qweatherAPIKey}`
+    )
+    .then((response) => {
+      if (response.data.code == 200) {
+        weatherDataHourly.value = response.data.hourly;
+        // console.log(weatherDataHourly.value);
+        weatherDataHourly.value.forEach((hour) => {
+          hour.fxTime = obsTimeFormat(hour.fxTime);
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return weatherDataHourly.value;
+};
+const weatherDataHourly = await getWeatherDataHourly();
 </script>
